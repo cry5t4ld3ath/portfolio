ab1bdc2bcf7c10a8fea2300e1d5c1ce672865cb5
sadeq <147354728+cry5t4ld3ath@users.noreply.github.com>
2024-07-02 01:19:34 +0330

    d

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -12,7 +12,7 @@ export default function IndexPage({params: {locale}}: Props) {
    unstable_setRequestLocale(locale);
 
    return (
-      <div className="min-h-screen bg-black w-full">
+      <div className="">
          <h1 className="text-[5rem] text-black"> hello</h1>
       </div>
    )
