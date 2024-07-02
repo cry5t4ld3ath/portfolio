31d1577d8710fe13e7bbcfdc7e35a5b19453f734
cry5t4ld3ath <sadeghsn76@gmail.com>
2024-07-02 03:47:24 +0330

    fg

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -4,7 +4,7 @@ export default function AboutPage() {
    return (
       <div className="h-[200vh] bg-green-950 text-black text-[5rem]">
          <div className="min-h-screen">
-            <h1 className="">{t('title')}</h1> 
+            <h1></h1> 
          </div>
       </div>
    )
