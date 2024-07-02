1138d7916535c5578c602a1124b49b6e53fb21a0
cry5t4ld3ath <sadeghsn76@gmail.com>
2024-07-02 20:59:58 +0330

    ddd

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -63,7 +63,7 @@ export default function MobileHeader () {
                      </NavigationLink>
                </div>
 
-               <div className="absolute top-5 left-5">
+               <div className="fixed top-5 left-5">
                   <LocaleLink defaultValue={locale}>
                      {locales.map((cur) => (
                         <button 
