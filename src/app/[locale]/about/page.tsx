e22297e5303512333dcb5b65a88f08a7896622ab
cry5t4ld3ath <sadeghsn76@gmail.com>
2024-07-02 14:03:51 +0330

    fgfg

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,7 +1,16 @@
 import { useTranslations } from "next-intl"
+import { unstable_setRequestLocale } from "next-intl/server";
 
 
-export default function AboutPage() {
+type Props = {
+   params: {locale: string};
+};
+
+
+export default function AboutPage({params: {locale}}: Props) {
+
+   unstable_setRequestLocale(locale)
+
 
    const t = useTranslations('about');
 
