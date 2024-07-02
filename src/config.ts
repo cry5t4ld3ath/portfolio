fad1f25c8c2b23b7d5432abec9ee668340a436dc
cry5t4ld3ath <sadeghsn76@gmail.com>
2024-07-02 03:49:50 +0330

    yuuytr

--- src/config.ts
+++ src/config.ts
@@ -6,10 +6,6 @@ export const locales = ['en', 'fa'] as const;
 export const pathnames: Pathnames<typeof locales> = {
   '/': '/',
   
-  '/about': {
-    en: '/about',
-    fa: '/درباره'
-  }
 };
 
 export const localePrefix: LocalePrefix<typeof locales> = 'always';
