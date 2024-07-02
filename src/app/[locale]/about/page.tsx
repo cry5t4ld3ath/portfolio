717348655377c3b5e2abd9ca24f8d78fd2040644
cry5t4ld3ath <sadeghsn76@gmail.com>
2024-07-02 13:30:27 +0330

    f

---
+++ src/app/[locale]/about/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function AboutPage() {
+
+
+   return (
+      <div className="min-h-screen bg-slate-900">
+         <h1 className="text-[5rem] text-red-900">about</h1>
+      </div>
+   )
+}
