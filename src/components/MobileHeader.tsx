859ea0b6216a193c5bd4f4b5edaff0107881f8d9
cry5t4ld3ath <sadeghsn76@gmail.com>
2024-07-02 18:19:35 +0330

    dfdfdfdf

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -25,42 +25,46 @@ export default function MobileHeader () {
       <div className="desktop:hidden fixed top-0 right-0">
 
          <div>
-               <div onClick={handleNav} className="w-fit h-fit fixed top-5 right-8 z-50">
-                  <button className="w-fit h-fit relative grid place-items-center">
-                     <span className={`w-[35px] h-[4px] absolute rounded-full bg-white transform duration-500 ease-in-out ${menuOpen ? "rotate-45 bg-red-800" : "-translate-y-1.5"} `} />
-                     <span className={`w-[25px] h-[4px] absolute rounded-full bg-white transform duration-500 ease-in-out ${menuOpen ? "opacity-0" : ""}`} />
-                     <span className={`h-[4px] absolute rounded-full bg-white transform duration-500 ease-in-out ${menuOpen ? "-rotate-45 bg-red-800 w-[35px]" : "translate-y-1.5 w-[15px]"}`} />
-                  </button>
-               </div>
+            
+            <div onClick={handleNav} className="w-fit h-fit fixed top-5 right-8 z-50">
+               <button className="w-fit h-fit relative grid place-items-center">
+                  <span className={`w-[35px] h-[4px] absolute rounded-full bg-white transform duration-500 ease-in-out ${menuOpen ? "rotate-45 bg-red-800" : "-translate-y-1.5"} `} />
+                  <span className={`w-[25px] h-[4px] absolute rounded-full bg-white transform duration-500 ease-in-out ${menuOpen ? "opacity-0" : ""}`} />
+                  <span className={`h-[4px] absolute rounded-full bg-white transform duration-500 ease-in-out ${menuOpen ? "-rotate-45 bg-red-800 w-[35px]" : "translate-y-1.5 w-[15px]"}`} />
+               </button>
+            </div>
 
             <div className={`
                fixed top-0 right-0 w-full min-h-screen backdrop-blur-sm bg-red-950/20 transition-all duration-500 
                ${menuOpen ? "opacity-100 visible" : "opacity-0 invisible"}`}
                >
-                  <div className="min-h-screen flex flex-col justify-center items-center font-[evance] text-[2rem]">
-                     <NavigationLink href='/'>
-                           Home
-                        </NavigationLink>
-                        <NavigationLink href='/about'>
-                           About
-                        </NavigationLink>
-                        <NavigationLink href='/projects'>
-                           Projects
-                        </NavigationLink>
+                  <div onClick={handleNav}>
+                     <div className="min-h-screen flex flex-col justify-center items-center font-[evance] text-[2rem]">
+                        <NavigationLink href='/'>
+                              Home
+                           </NavigationLink>
+                           <NavigationLink href='/about'>
+                              About
+                           </NavigationLink>
+                           <NavigationLink href='/projects'>
+                              Projects
+                           </NavigationLink>
+                     </div>
+
+                     <div className="fixed bottom-5 right-10 border-red-900 border rounded-2xl">
+                        <LocaleLink defaultValue={locale}>
+                           {locales.map((cur) => (
+                              <button 
+                              key={cur} 
+                              value={cur} 
+                              className={`mx-4 my-2 ${cur === locale ? ' text-white' : 'text-red-600'}`}>
+                              {t(cur)}
+                              </button>
+                           ))}
+                        </LocaleLink>
+                     </div>
                   </div>
 
-                  <div className="fixed bottom-5 right-10 border-red-900 border rounded-2xl">
-                     <LocaleLink defaultValue={locale}>
-                        {locales.map((cur) => (
-                           <button 
-                           key={cur} 
-                           value={cur} 
-                           className={`mx-4 my-2 ${cur === locale ? ' text-white' : 'text-red-600'}`}>
-                           {t(cur)}
-                           </button>
-                        ))}
-                     </LocaleLink>
-                  </div>
                   
             </div>
 
