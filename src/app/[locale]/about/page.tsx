411d0c10560fd8cfec9a8068aa525331f9d15971
cry5t4ld3ath <sadeghsn76@gmail.com>
2024-07-02 04:13:00 +0330

    ;;;;;;;;;;jjjjj

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,34 +1,13 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
 import { useTranslations } from "next-intl";
 import { getLangDir } from "rtl-detect";
 
-type Params = {
-  locale: string;
-};
-
 type Props = {
-  locale: string;
-};
-
-export const getStaticPaths: GetStaticPaths<Params> = async () => {
-  return {
-    paths: [
-      { params: { locale: 'en' } },
-      { params: { locale: 'fa' } }
-    ],
-    fallback: false
+  params: {
+    locale: string;
   };
 };
 
-export const getStaticProps: GetStaticProps<Props, Params> = async ({ params }) => {
-  return {
-    props: {
-      locale: params!.locale
-    }
-  };
-};
-
-export default function AboutPage({ locale }: Props) {
+export default function AboutPage({ params: { locale } }: Props) {
   const t = useTranslations('about');
   const direction = getLangDir(locale);
 
