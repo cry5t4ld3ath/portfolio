e6115b43387d4ec214a8119e8d2d7a73461bf486
cry5t4ld3ath <sadeghsn76@gmail.com>
2024-07-08 15:12:13 +0330

    gfgfgf

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -34,19 +34,21 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
-      <Loading>
-      <body className="grid desktop:grid-cols-[1.7fr_0.4fr] mobile:grid-cols-1 mobile:grid-rows-1">
+      <body>
         <NextIntlClientProvider messages={messages}>
-          
+          <Loading>
             <main>
               {children}
             </main>
             <Header />
             <MobileHeader />
-          
+          </Loading>
         </NextIntlClientProvider>
       </body>
-      </Loading>
     </html>
   );
 }
+
+
+
+// className="grid desktop:grid-cols-[1.7fr_0.4fr] mobile:grid-cols-1 mobile:grid-rows-1"
