bd26d5830cf2cca5b389c5d3f6898b670d24300f
sadeq <147354728+cry5t4ld3ath@users.noreply.github.com>
2024-07-10 13:01:27 +0330

    tytyth

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,10 +8,10 @@ export default function Header() {
 
 
    return (
-      <header className="min-h-screen desktop:fixed top-0 right-0 w-[15vw] mobile:hidden z-50">
-         <nav className="h-full w-full overflow-hidden">
+      <header className="min-h-screen desktop:fixed top-0 right-0 w-[20vw] mobile:hidden z-50">
+         <nav className="h-full w-full">
             <div>
-               <div className="min-h-screen w-full px-5 text-[3rem] flex justify-end">
+               <div className="min-h-screen w-full px-2 text-[3rem] flex justify-end">
                   <div className="space-y-20 flex flex-col items-end justify-center font-[evance]">
                      <NavigationLink href='/'>
                         Home
