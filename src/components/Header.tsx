702ed0fd068c51db7d0e1549772336c426d97cc0
sadeq <147354728+cry5t4ld3ath@users.noreply.github.com>
2024-07-10 13:06:20 +0330

    rtrtrt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
 
 
    return (
-      <header className="min-h-screen desktop:fixed top-0 right-0 w-[20vw] mobile:hidden z-50">
+      <header className="min-h-screen desktop:fixed top-0 right-0 w-[15vw] mobile:hidden z-50">
          <nav className="h-full w-full">
             <div>
                <div className="min-h-screen w-full px-2 text-[3rem] flex justify-end">
