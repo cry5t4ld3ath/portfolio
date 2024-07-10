b8f0c67b88371ee7a70c865836fab6478d3131fc
sadeq <147354728+cry5t4ld3ath@users.noreply.github.com>
2024-07-10 21:09:33 +0330

    fgfgggg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,59 @@
+"use client"
+
+
+import React, { useEffect, useRef } from "react";
 import LocaleSwitcher from "./LocaleSwitcher";
 import NavigationLink from "./NavigationLink";
+import gsap from "gsap";
 
 
 
-export default function Header() {
+const Header : React.FC = () => {
 
+   const headerRef = useRef(null);
+ 
+   useEffect(() => {
+
+      const hasLoaded = sessionStorage.getItem('hasLoaded');
+     
+      if (!hasLoaded) {
+         const header = headerRef.current;
+
+         gsap.set(header, { opacity: 0, x: 100 });
+
+         const tl = gsap.timeline({ delay: 2.5 });
+
+         tl.to(header, {
+            duration: 1,
+            opacity: 1,
+            x: 0,
+            ease: "power3.out"
+         })
+
+         
+         sessionStorage.setItem('hasLoaded', 'true');
+      }
+ 
+
+   }, []);
 
 
    return (
-      <header className="min-h-screen desktop:fixed top-0 right-0 w-[15vw] mobile:hidden z-50">
+      <header 
+      ref={headerRef}
+      className="min-h-screen desktop:fixed top-0 right-0 w-[15vw] mobile:hidden z-50">
          <nav className="h-full w-full">
             <div>
                <div className="min-h-screen w-full px-2 text-[3rem] flex justify-end">
                   <div className="space-y-20 flex flex-col items-end justify-center font-[evance]">
-                     <NavigationLink href='/'>
-                        Home
-                     </NavigationLink>
-                     <NavigationLink href='/about'>
-                        About
-                     </NavigationLink>
-                     <NavigationLink href='/projects'>
-                        Projects
-                     </NavigationLink>
+                     {['home' , 'about' , 'projects'].map((item) => (
+                        <NavigationLink 
+                        key={item}
+                        href={item === 'home' ? '/' : `/${item}`} 
+                        >
+                           {item}
+                        </NavigationLink>
+                     ))}
                   </div>
                </div>
                <div className="fixed bottom-0 right-5">
@@ -32,3 +64,5 @@ export default function Header() {
       </header>
    )
 }
+
+export default Header;
