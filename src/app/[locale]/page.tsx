ca51406657dc7ca95dd336bf29fbaabb805fe7a5
cry5t4ld3ath <sadeghsn76@gmail.com>
2024-07-14 20:15:47 +0330

    dfdfdf

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,10 @@
 import { unstable_setRequestLocale } from "next-intl/server";
 import { useTranslations } from "next-intl";
 import StarsCanvas from "@/components/particles";
+import ParallaxEffect from "@/components/parallaxE";
+import GlowEffect from "@/components/Glow";
+import React from "react";
+
 
 type Props = {
    params: {locale: string};
@@ -15,9 +19,15 @@ export default function IndexPage({params: {locale}}: Props) {
    
 
    return (
-      <div className="h-[200dvh] w-full">
+      <div className="min-h-screen w-full grid place-items-center">
          <StarsCanvas />
-         <h1 className="text-[5rem] text-white">{t("about")}</h1>
+         <ParallaxEffect>
+            <GlowEffect>
+            <h1 className="text-[5rem] text-nowrap text-stone-600 pointer-events-none glow glow:text-white glow:drop-shadow-[5px_5px_4px_rgba(255,0,0,1)]">{t("about")}</h1>
+               <div className="glow-overlay" style={{"--glow-color" : "#ff0000"} as React.CSSProperties} />
+            </GlowEffect>
+         </ParallaxEffect>
+         <h1 className="text-[3rem] text-stone-600 pointer-events-none desktop:hidden">{t("about")}</h1>
       </div>
    )
 }
